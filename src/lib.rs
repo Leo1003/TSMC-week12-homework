bd69fc074317340103810ff873c8f5b7fb2787f2
Leo <leo881003@gmail.com>
2022-05-15 16:24:23 +0800

    Add simple discounts tests

--- src/lib.rs
+++ src/lib.rs
@@ -21,4 +21,12 @@ mod tests {
         assert_eq!(8.0, price(&[4]));
         assert_eq!(24.0, price(&[1, 1, 1]));
     }
+
+    #[test]
+    fn simple_discounts() {
+        assert_eq!(15.2, price(&[0, 1]));
+        assert_eq!(21.6, price(&[0, 2, 4]));
+        assert_eq!(25.6, price(&[0, 1, 2, 4]));
+        assert_eq!(30.0, price(&[0, 1, 2, 3, 4]));
+    }
 }
