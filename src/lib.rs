fffc4eb93b0b12361c3dd88df8b48782bc77a11c
Leo <leo881003@gmail.com>
2022-05-15 16:17:10 +0800

    Add basic tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,18 @@
+pub fn price(books: &[u8]) -> f64 {
+    0.0
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn basic() {
+        assert_eq!(0.0, price(&[]));
+        assert_eq!(8.0, price(&[1]));
+        assert_eq!(8.0, price(&[2]));
+        assert_eq!(8.0, price(&[3]));
+        assert_eq!(8.0, price(&[4]));
+        assert_eq!(24.0, price(&[1, 1, 1]));
     }
 }
